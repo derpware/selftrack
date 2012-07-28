ef5f69651617ebc91174434fee4e42487de202da
Severin Schols <severin@schols.de>
2012-07-28 13:43:11 +0200

    Added memory, swap, and processes

--- selftrack.py
+++ selftrack.py
@@ -6,6 +6,7 @@ from time import sleep
 from json import load
 import config
 import eeml
+import subprocess
 
 def noOfOpenTabs():
 	json_data=open(config.MOZILLA_SESSIONSTORE)
@@ -27,6 +28,18 @@ def loadavg():
 	loadavg = inputFile.read().split()[0]
 	inputFile.close()
 	return loadavg
+	
+def memory():
+	memory = subprocess.check_output("free -m | grep Mem | awk '{print $3}'", shell=True)
+	return memory
+
+def swap():
+	swap = subprocess.check_output("free -m | grep Swap | awk '{print $3}'", shell=True)
+	return swap
+
+def processes():
+	processes = subprocess.check_output("ps aux | wc -l", shell=True)
+	return processes
 
 feed = eeml.Pachube(config.API_URL, config.API_KEY)
 while 1 :
@@ -35,6 +48,10 @@ while 1 :
 	update_data.append(eeml.Data("browser.tabs", noOfOpenTabs()))
 	update_data.append(eeml.Data("cpu.temperature", temperature(), unit=eeml.Celsius()))
 	update_data.append(eeml.Data("cpu.loadavg", loadavg()))
+	update_data.append(eeml.Data("os.memory", memory()))
+	update_data.append(eeml.Data("os.swap", swap()))
+	update_data.append(eeml.Data("os.processes", processes()))
+	
 
 	feed.update(update_data)
 	try:
